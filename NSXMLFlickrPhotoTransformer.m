211b4b2ec77fba345bdb00df7628f24bcd740f82
Wesley Moore <wez@wezm.net>
2010-01-28 21:03:24 +1100

    Add FlickrPhotoTransformer for generating Atom from Photos

---
+++ NSXMLFlickrPhotoTransformer.m
@@ -0,0 +1,14 @@
+//
+//  NSXMLFlickrPhotoTransformer.m
+//  monothumb
+//
+//  Created by Wesley Moore on 28/01/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "NSXMLFlickrPhotoTransformer.h"
+
+
+@implementation NSXMLFlickrPhotoTransformer
+
+@end
