8b40d3a9c69432c2519a5c9e3665b6fe64609799
Wesley Moore <wez@wezm.net>
2010-01-29 13:54:12 +1100

    Switch to tree-based XML parser

--- monothumb.m
+++ monothumb.m
@@ -1,24 +1,67 @@
 #import <Foundation/Foundation.h>
-#import "PhotoXMLDelegate.h"
+
+void process_photo_node(NSXMLNode *node);
 
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
+	NSError *errors;
 	NSURL *api_url = [NSURL URLWithString:@"http://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=aa003631cc50bd47f27f242d30bcd22f&user_id=40215689%40N00&per_page=20&extras=url_sq,url_m"];
-	NSXMLParser *parser = [[NSXMLParser alloc] initWithContentsOfURL:api_url];
+	NSXMLDocument *xml = [[NSXMLDocument alloc] initWithContentsOfURL:api_url options:NSXMLNodeOptionsNone error:&errors];
+	NSXMLElement *e;
+	NSString *stat;
+	NSArray *photo_nodes;
 	
-	//NSAssert(parser != nil, @"Parser is nil");
-	
-	PhotoXMLDelegate *xml_delegate = [[PhotoXMLDelegate alloc] init];
-	[parser setDelegate:xml_delegate];
-	[parser setShouldProcessNamespaces:NO];
-	[parser setShouldReportNamespacePrefixes:NO];
-	[parser setShouldResolveExternalEntities:NO];
+	if(xml == nil) {
+		NSLog(@"Error parsing the photo XML");
+		[pool drain];
+		return 1;
+	}
 
+	e = [xml rootElement];
+	stat = [[e attributeForName:@"stat"] stringValue];
+	if(stat == nil) {
+		NSLog(@"Expected stat attribute but got nil");
+		[xml release];
+		[pool drain];
+		return 1;
+	}
 	
-	
-	[parser parse];
+	if ([stat compare:@"ok"] != NSOrderedSame) {
+		NSLog(@"Stat not ok: %@", stat);
+		[xml release];
+		[pool drain];
+		return 1;
+	}
+
+	photo_nodes = [xml nodesForXPath:@"/res/photos/photo" error:&errors];
+	if(errors != nil) {
+		NSLog(@"Error getting photos");
+		[xml release];
+		[pool drain];
+		return 1;
+	}
+
+	[photo_nodes enumerateObjectsWithOptions:NSEnumerationConcurrent usingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+		NSXMLNode *node = (NSXMLNode *)obj;
+		process_photo_node(node);
+	}];
 	
     [pool drain];
     return 0;
+}
+
+void process_photo_node(NSXMLNode *node)
+{
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+
+	if ([node kind] != NSXMLElementKind) {
+		NSLog(@"Expecting XML element, got something else");
+		return;
+	}
+	NSXMLElement *photo = (NSXMLElement *)node;
+	
+	NSString *value = [[photo attributeForName:@"url_sq"] stringValue];
+	
+	[pool drain];
 }
